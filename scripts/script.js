0fd013358bcc68e6af4b958fb6bde15f56c41f50
Xander Gardner <xandergardner7@gmail.com>
2023-01-27 18:17:40 -0500

    2022 bob

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-pics = document.querySelectorAll(".oriShow");
-pics.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.parentNode.style.backgroundImage = "url('')";
-    item.style.opacity = 1.0;
-  });
-});
